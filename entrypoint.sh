261a2be91ca7aa127f1d874562e78286578d2494
robinkonrad <github@robinkonrad.de>
2022-11-07 16:41:50 +0100

    detail work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-INPUT_FILENAME="${INPUT_FILENAME:-$2}"
+INPUT_FILENAME="${INPUT_FILENAME:-$1}"
+INPUT_FILENAME="${INPUT_FILENAME:-azureupdatesfeed.json}"
 INPUT_FILENAME="${GITHUB_WORKSPACE}/data/${INPUT_FILENAME}"
 mkdir -p "$(dirname "$INPUT_FILENAME")"
 rm -f "$INPUT_FILENAME"
 echo "::debug::Location of the converted xml2json file: $INPUT_FILENAME"
 
 INPUT_ROWFILENAME="${INPUT_ROWFILENAME:-$2}"
+INPUT_ROWFILENAME="${INPUT_ROWFILENAME:-azureupdatesfeed.xml}"
 INPUT_ROWFILENAME="${GITHUB_WORKSPACE}/data/${INPUT_ROWFILENAME}"
 mkdir -p "$(dirname "$INPUT_ROWFILENAME")"
 rm -f "$INPUT_ROWFILENAME"
-echo "::debug::Location of the converted xml2json file: $INPUT_ROWFILENAME"
+echo "::debug::Location of the raw xml2json file: $INPUT_ROWFILENAME"
 
 FEEDURL="https://azurecomcdn.azureedge.net/en-us/blog/topics/updates/feed/"
 echo "::debug::Feed url: $FEEDURL"
